17ce1f2e8ffcf34b1566ccbc59a5d5e65303d5b3
Myasnikov Alexey <amyasnikov@dev.pkb-rio.com>
2019-09-10 10:29:48 +0300

    add client code

--- client/Client.h
+++ client/Client.h
@@ -6,15 +6,35 @@
  * @copyright (C) 2019
  */
 
+#include <string.h>
+#include <bits/types/FILE.h>
 
 #pragma once
 
 namespace berkeley
 {
 
+/**
+ * Class describes client
+ */
 class Client
 {
+public:
+    explicit Client(unsigned int port);
 
+    /**
+     * Initialize client
+     */
+    void init();
+
+    /**
+     * Process method
+     */
+    void process(FILE *fp, int descriptor);
+
+private:
+
+    unsigned int m_port;
 };
 
 } // berkeley
--- client/main.cpp
+++ client/main.cpp
@@ -8,9 +8,16 @@ using namespace berkeley;
 
 constexpr auto DEFAULT_PORT = 9998;
 
+std::shared_ptr<Client> createClient()
+{
+    return std::make_shared<Client>(DEFAULT_PORT);
+}
+
 } // anonymous
 
 int main(int argc, char *argv[])
 {
+    auto client = createClient();
+    client->init();
     return 0;
 }
